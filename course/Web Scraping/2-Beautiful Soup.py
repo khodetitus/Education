257486e2461c06a70e19ec13e195a65a14cda25b
khodetitus <masoudpro2@gmail.com>
2022-09-30 05:41:43 +0330

    2-Beautiful Soup.py | modify file | khodetitus

--- course/Web Scraping/2-Beautiful Soup.py
+++ course/Web Scraping/2-Beautiful Soup.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+# Example 1
 url = "https://en.wikipedia.org/wiki/Mao_Zedong"
 
 response = requests.get(url)
@@ -25,3 +26,9 @@ print(content.find(re.compile("^d")))
 
 # 4.elect method
 print(content.select("li>a[title=Benjamin Tucker]"))
+
+# Example 2
+bs = BeautifulSoup("<h1>Hello World!</h1>")
+print(bs.prettify())
+print(bs.find(text="Hello World!"))
+print(bs.find_all(name="h1"))
