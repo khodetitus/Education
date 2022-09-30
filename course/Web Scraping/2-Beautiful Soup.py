53122d5558cbe74630448a8cdeecf1c5e5170516
khodetitus <masoudpro2@gmail.com>
2022-09-30 04:50:42 +0330

    2-Beautiful Soup.py | modify file | khodetitus

---
+++ course/Web Scraping/2-Beautiful Soup.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+url = "https://en.wikipedia.org/wiki/Mao_Zedong"
+
+response = requests.get(url)
+content = BeautifulSoup(response.text, "html.parser")
+
+# 1.find method
+print(content.find("h1"))
+print(content.find("h2").get("id"))
+print(content.find("h1").attrs)
+print(content.find("h1").text)
+print(content.find("h1").name)
+print(content.find(attrs={"role": "main"}))
+print(content.find(class_="mv_indicator"))
+
+# 2.find_all method
+print(content.find_all(["h1", "h2"]))
+print(content.find_all("h2", limit=5))
+
+# 3.find with regex
+print(content.find(re.compile("^d")))
+
+# 4.elect method
+print(content.select("li>a[title=Benjamin Tucker]"))
