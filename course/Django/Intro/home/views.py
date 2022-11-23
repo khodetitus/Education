5e48b26cf1993d6a0e0f2daaa55e9b1f19007f67
khodetitus <masoudpro2@gmail.com>
2022-11-23 05:52:48 +0330

    views.py  | modify file | khodetitus

--- course/Django/Intro/home/views.py
+++ course/Django/Intro/home/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
+from django.contrib import messages
 
 
 # Create your views here.
@@ -9,9 +10,12 @@ def home(request):
     return render(request, 'home.html', {"all": all})
 
 
-def say_hello(request):
-    person = {"name": "masoud"}
-    return render(request, 'hello.html', context=person)
+def details(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    return render(request, 'details.html', {"todo": todo})
 
 
-
+def delete(request, todo_id):
+    Todo.objects.get(id=todo_id).delete()
+    messages.success(request, "Todo Deleted Successfully", extra_tags="success")
+    return redirect('home')
